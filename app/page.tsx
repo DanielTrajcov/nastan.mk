aa82e1698017d5c30f95e6838d647aa47100ea57
DanielTrajcov <danieltrajcov@gmail.com>
2024-12-17 01:28:52 +0100

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -37,8 +37,6 @@ export default function Home() {
           setZipCode={setZipCode}
         />
 
-        <GameList />
-
         <Posts
           searchResults={searchResults}
           zipCode={zipCode}

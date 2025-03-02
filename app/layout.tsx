5e33abc5cdc4cb5a218384aad62c72cc2654917e
Daniel Trajchov <danieltrajcov@gmail.com>
2025-03-02 22:11:13 +0100

    2.062

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Provider from "./Provider";
 import Header from "./components/Header";
